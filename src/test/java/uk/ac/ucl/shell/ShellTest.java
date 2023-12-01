7fbc60ec4aee5d69838cf420ef561ceeab6bddb6
Noah Geerts <noahgeerts@outlook.com>
2023-12-01 11:47:14 +0000

    added setup and teardown

--- src/test/java/uk/ac/ucl/shell/ShellTest.java
+++ src/test/java/uk/ac/ucl/shell/ShellTest.java
@@ -1,58 +1,77 @@
 package uk.ac.ucl.shell;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class ShellTest {
-
-    @Test
-    public void testHead() throws IOException {
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        OutputStreamWriter writer = new OutputStreamWriter(outputStream);
-
-        List<String> appArgs = new ArrayList<>(Arrays.asList("5", "testHead.txt"));
-        String input = "";
-        // it is testing apps through creating an instance,
-        // not actually running the shell
-        Head head = new Head();
-        head.exec(new ArrayList<>(appArgs), input, writer);
-
-        String expectedOutput = "line 1\nline 2\nline 3\nline 4\nline 5\n";
-        assertEquals(expectedOutput, outputStream.toString());
+	
+	static String directoryPath = "testDirectory";
+	static String testGrepPath = directoryPath + "/" + "testGrep.txt";
+	static String abcPath = directoryPath + "/abc.txt";
+	
+	@Before
+    public void setUp() throws IOException {
+        //set up test directory
+        Path path = Paths.get(directoryPath);
+        Path subPath = Paths.get(directoryPath + "/subDirectory");
+        Files.createDirectory(path);
+        Files.createDirectory(subPath);
+        
+        //set up test files
+        FileWriter writer = new FileWriter(testGrepPath);
+        writer.write("This is a line containing the pattern.\nThis is a line that doesn't.");
+        writer.close();
+        
+        writer = new FileWriter(abcPath);
+        writer.write("AAA\nBBB\nCCC");
+        writer.close();
     }
 
-    @Test
-    public void testGrep() throws IOException {
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        OutputStreamWriter writer = new OutputStreamWriter(outputStream);
-
-        List<String> appArgs = new ArrayList<>(Arrays.asList("pattern", "testGrep.txt"));
-        String input = "";
-
-        Grep grep = new Grep();
-        grep.exec(new ArrayList<>(appArgs), input, writer);
-
-        String expectedOutput = "This is a line containing the pattern.\n";
-        assertEquals(expectedOutput, outputStream.toString());
+    @After
+    public void tearDown() {
+    	//delete test directory and all test files
+        try {
+            Path directory = Paths.get(directoryPath);
+            Files.walk(directory)
+                 .sorted((p1, p2) -> -p1.compareTo(p2)) // Reverse order for directories first
+                 .forEach(p -> {
+                     try {
+                         Files.delete(p);
+                     } catch (IOException e) {
+                         System.err.println("Failed to delete: " + p + " - " + e.getMessage());
+                     }
+                 });
+        } catch (IOException e) {
+            System.err.println("Failed to delete directory: " + e.getMessage());
+        }
     }
-
+    
     @Test
-    public void testCut() throws IOException {
-        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-        OutputStreamWriter writer = new OutputStreamWriter(outputStream);
-
-        List<String> appArgs = new ArrayList<>(Arrays.asList("-b", "1-3", "testCut.txt"));
-        String input = "";
-
-        Cut cut = new Cut();
-        cut.exec(new ArrayList<>(appArgs), input, writer);
-
-        String expectedOutput = "lin\ntex\nt\n";
-        assertEquals(expectedOutput, outputStream.toString());
+    public void testLsFoundFilesAndDirectory() throws IOException {
+    	Application Ls = new Ls();
+    	ByteArrayOutputStream capture = new ByteArrayOutputStream();
+    	OutputStreamWriter writer = new OutputStreamWriter(capture);
+    	Ls.exec(new ArrayList<String>(), "", writer);
+    	writer.flush();
+    	writer.close();
+    	
+    	String output = writer.toString();
+    	String expected = "subDirectory\ntestGrep.txt\nabc.txt\n";
+    	
+    	assertTrue(output.equals(expected));
     }
+    
+    
+   
 }
